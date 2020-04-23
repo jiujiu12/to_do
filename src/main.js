d680aba24e183ea4c6d83857ce39cad47a65956b
furry <18834176546@163.com>
2020-04-23 22:29:35 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import ElementUI from 'element-ui'
+import Vuetify from 'Vuetify'
 import App from './App'
 import router from './router'
 
+// Vue.use(ElementUI);
+Vue.use(Vuetify);
+
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
